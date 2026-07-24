5f2a25d1cc7c695ed58e94ac4e560ad292235938
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2026-07-24 12:31:24 +0100

    routing
    Create routing system

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/home/home';
+import { UserSelector } from './features/user-selector/user-selector';
+import { AppointmentForm } from './features/appointment-form/appointment-form';
+import { AppointmentSearch } from './features/appointment-search/appointment-search';
+import { AdminDashboard } from './features/admin-dashboard/admin-dashboard';
+import { Notfound } from './features/notfound/notfound';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'' ,component:Home,title:'Accueil'
+  },
+  {
+    path:'profiles',component:UserSelector,title:'Choisir un profile'
+  },
+  {
+    path:'request',component:AppointmentForm,title:'nouvelle demande'
+  },
+  {
+    path:'search',component:AppointmentSearch, title:"recherchez mes rendez-vous"
+  },
+  {
+    path:'admin',component:AdminDashboard, title:'Administration'
+  },
+  {
+    path:"**", component: Notfound,title: 'page not found'
+  }
+];
