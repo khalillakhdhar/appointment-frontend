4512030bc024cc44cfc15d2f7ee29c3247cc8a62
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2026-07-27 10:30:31 +0100

    application

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { UserStorageService } from '../../core/services/user-storage-service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+    readonly userStorage = inject(UserStorageService);
+
+}
--- src/app/features/user-selector/user-selector.ts
+++ src/app/features/user-selector/user-selector.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { UserStorageService } from '../../core/services/user-storage-service';
+import { Router } from '@angular/router';
+import { User } from '../../core/models/user.model';
 
 @Component({
   selector: 'app-user-selector',
@@ -6,4 +9,13 @@ import { Component } from '@angular/core';
   templateUrl: './user-selector.html',
   styleUrl: './user-selector.css',
 })
-export class UserSelector {}
+export class UserSelector {
+  readonly userStorage = inject(UserStorageService);
+  private readonly router = inject(Router);
+
+  select(user: User): void {
+    this.userStorage.SelectUser(user);
+    void this.router.navigateByUrl(user.role === 'admin' ? '/admin' : '/');
+  }
+
+}
